0518afe1a1d855ba851e8d8d1b7a0e7ae460ff96
luifa7 <luifa.luifa@gmail.com>
2022-01-29 13:16:34 +0100

    feat: add update dtos

---
+++ DTOs/Recommendations/UpdateRecommendation.cs
@@ -0,0 +1,18 @@
+﻿namespace DTOs.Recommendations
+{
+    public class UpdateRecommendation
+    {
+        public string PlaceName { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Address { get; set; }
+        public string Maps { get; set; }
+        public string Website { get; set; }
+        public string Instagram { get; set; }
+        public string Facebook { get; set; }
+        public string OtherLink { get; set; }
+        public string Photo { get; set; }
+        public string CityDId { get; set; }
+        public string[] Tags { get; set; }
+    }
+}
